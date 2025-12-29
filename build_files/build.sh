3ebb82050e2ef12d76b1e564b9b6bf1c916f250d
szenesis <slypotter7@gmail.com>
2025-12-28 19:09:02 -0600

    Packages cleanup and changed termonal from tilix to sakura

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,8 @@
 
 set -ouex pipefail
 
+systemctl enable systemd-timesyncd
+systemctl enable systemd-resolved.service
 
 ### Install packages
 
@@ -10,25 +12,39 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+
+#Flatpak installs
+flatpak install -y com.vivaldi.Vivaldi
+
 # Install terminal software from fedora repos
-dnf5 install -y tilix
-dnf5 install -y fish
-dnf5 install -y docker
+dnf5 install -y \ 
+ sakura \
+ fish \
+ docker \
+ fastfetch \
+ fzf \
+ 
 
 #Remove software that is not needed for workflow.
-dnf remove -y gnome-software
-dnf remove -y gnome-tour
-dnf remove -y gnome-system-monitor
-dnf remove -y yelp
+dnf remove -y \
+ gnome-software \
+ gnome-tour \
+ gnome-system-monitor \
+ yelp \
+ htop \
+ nvtop \
+ firefox \
 
 #Remove old/retro Gnome extensions
-dnf remove -y gnome-shell-extension-common
-dnf remove -y gnome-shell-extension-apps-menu-
-dnf remove -y gnome-shell-extension-launch-new-instance
-dnf remove -y gnome-shell-extension-places-menu
-dnf remove -y gnome-shell-extension-window-list
-dnf remove -y gnome-shell-extension-background-logo
-dnf remove -y gnome-shell-extension-appindicator
+dnf remove -y \ 
+ gnome-shell-extension-common \
+ gnome-shell-extension-apps-menu- \
+ gnome-shell-extension-launch-new-instance \
+ gnome-shell-extension-places-menu \
+ gnome-shell-extension-window-list \
+ gnome-shell-extension-background-logo \
+ gnome-shell-extension-appindicator \
+ gnome-extensions-app \
 
 # Install VS Code
 tee /etc/yum.repos.d/vscode.repo <<'EOF'
@@ -54,4 +70,55 @@ dnf -y install --enablerepo=code \
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+
+if [ "$(arch)" != "aarch64" ] ; then
+  dnf install -y \
+    virtualbox-guest-additions \
+    thermald
+fi
+
+# Saves a ton of space
+# Got from Zirconium gotta verify
+rm -rf /usr/share/doc
+rm -rf /usr/bin/chsh # footgun
+
+
+HOME_URL="https://github.com/szenesis/mercuryos"
+echo "Mercurium" | tee "/etc/hostname"
+# OS Release File (changed in order with upstream)
+# TODO: change ANSI_COLOR if applicable
+sed -i -f - /usr/lib/os-release <<EOF
+s|^NAME=.*|NAME=\"Mercurium\"|
+s|^PRETTY_NAME=.*|PRETTY_NAME=\"Mercurium\"|
+s|^VERSION_CODENAME=.*|VERSION_CODENAME=\"social possum\"|
+s|^VARIANT_ID=.*|VARIANT_ID=""|
+s|^HOME_URL=.*|HOME_URL=\"${HOME_URL}\"|
+s|^BUG_REPORT_URL=.*|BUG_REPORT_URL=\"${HOME_URL}/issues\"|
+s|^SUPPORT_URL=.*|SUPPORT_URL=\"${HOME_URL}/issues\"|
+s|^CPE_NAME=\".*\"|CPE_NAME=\"cpe:/o:szenesis:mercuryos\"|
+s|^DOCUMENTATION_URL=.*|DOCUMENTATION_URL=\"${HOME_URL}\"|
+s|^DEFAULT_HOSTNAME=.*|DEFAULT_HOSTNAME="merucurium"|
+
+/^REDHAT_BUGZILLA_PRODUCT=/d
+/^REDHAT_BUGZILLA_PRODUCT_VERSION=/d
+/^REDHAT_SUPPORT_PRODUCT=/d
+/^REDHAT_SUPPORT_PRODUCT_VERSION=/d
+EOF
+
+# Remove annyoing fedora flatpaks
+rm -rf /usr/lib/systemd/system/flatpak-add-fedora-repos.service
+systemctl enable flatpak-add-flathub-repos.service
+
+# Add Flathub to the image for eventual application (got from Zirconium)
+mkdir -p /etc/flatpak/remotes.d/
+curl --retry 3 -Lo /etc/flatpak/remotes.d/flathub.flatpakrepo https://dl.flathub.org/repo/flathub.flatpakrepo
+
+if [ "$(rpm -E "%{fedora}")" == 43 ] ; then
+  dnf -y copr enable ublue-os/flatpak-test
+  dnf -y copr disable ublue-os/flatpak-test
+  dnf -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak flatpak
+  dnf -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak-libs flatpak-libs
+  dnf -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak-session-helper flatpak-session-helper
+  rpm -q flatpak --qf "%{NAME} %{VENDOR}\n" | grep ublue-os
+fi
 
