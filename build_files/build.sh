04039d7b98959fe63ab8dc114a6f74d0743bca3f
szenesis <slypotter7@gmail.com>
2025-12-28 21:16:55 -0600

    more fixes

--- build_files/build.sh
+++ build_files/build.sh
@@ -23,9 +23,7 @@ mkdir -p /etc/flatpak/remotes.d/
 curl --retry 3 -Lo /etc/flatpak/remotes.d/flathub.flatpakrepo https://dl.flathub.org/repo/flathub.flatpakrepo
 
 #Bazaar store
-flatpak remote-add --system --if-not-exists flathub-beta \
-  https://flathub.org/beta-repo/flathub-beta.flatpakrepo
-flatpak install -y --system flathub-beta org.ublue.bazaar
+flatpak install flathub io.github.kolunmi.Bazaar
 
 #Flatpak browser install
 flatpak install -y com.vivaldi.Vivaldi
